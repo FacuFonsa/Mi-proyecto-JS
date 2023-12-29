c0ebd27f77634f7fc95c6d6732af5eea2b5b44a0
facundo fonsalido <fonsalidofacundo@gmail.com>
2023-12-29 18:26:06 -0300

    proyecto final listo

--- js/index.js
+++ js/index.js
@@ -1,134 +1,8 @@
-//verifiquemos tu edad//
-
-// const EDAD = prompt ("Antes de ingresar, verifiquemos tu edad")
-
-
-
-// const EDAD_NUMERO = parseInt(EDAD);
-
-
-
-// if (isNaN(EDAD_NUMERO)){ 
-//     console.log("Ingresa solo numeros");
-// } else{
-//     if (EDAD_NUMERO >=18){
-//         console.log("Perfecto, ahora podes ingresar tu email");
-//     } else{
-//         console.log("Perdon, pero sos menor para ingresar");
-//     }
-
-// }
-
-
-// //ingresa al sitio//
-
-
-// let nombreIngresado = prompt("ingresar nombre:");
-// let apellidoIngresado = prompt("ingresa tu apellido");
-
-// function saludar(nombre, apellido) {
-//     console.log("bienvenido " + nombre + " " + apellido);
-// }
-
-// saludar(nombreIngresado, apellidoIngresado);
-
-
-// let email = prompt("ingresar tu email:");
-// let contraseña = prompt("ingresa tu contraseña");
-
-// function saludar2(email, contraseña) {
-//     console.log("Tus datos son: " + email+ " " + contraseña);
-// }
-
-// saludar2(email, contraseña);
-
-// //////////////////////////////////////////////////////////////////////////
-// //array con informacion de usurario
-
-// function Usuario(nombre, apellido, fecha_de_ingreso){
-//     this.nombre = nombre
-//     this.apellido = apellido
-//     this.fecha_de_ingreso = fecha_de_ingreso
-// }
-
-// const USUARIO = new Usuario("Facundo", "Fonsalido", 2023);
-// console.log(USUARIO);
-// //////////////////////////////////////////////////////////////////////////
-
-
-
-// //calcula el precio de tu set de bateria//
-
-// function sumar() {
-//     let num1= parseFloat(prompt("Ingresa el precio de Platillos"));
-//     let num2= parseFloat(prompt("ingrese el precio de Palillos"));
-//     let suma= num1 + num2
-//     alert("Tu nuevo set de Bateria cuesta: " + " " + "$" + suma)
-//     console.log("la suma es" + " " + suma);
-// }
-
-
-// sumar();
-
-
-// //////////////////////////////////////////////////////////////////////
-
-// //carrito de productos
-// const CARRITO =[
-//     {
-//         producto: "Platillos",
-//         precio: 350000
-//     },
-//     {
-//         producto: "Palilloa",
-//         precio: 75000
-//     },    
-// ]
-
-
-// let precioTotal = 0;
-// for(const item of CARRITO){
-//     precioTotal += item.precio;
-// }
-
-// console.log("el precio total del carrito es : $" + precioTotal);
-// //////////////////////////////////////////////////////////////////////
-
-// //Carrito de productos ordenados por valor
-// const CARRITO2 = [
-//     {producto: "Platillos", precio:350000},
-//     {producto: "Palillos", precio:75000},
-// ]
-
-// let productosOrdenados = CARRITO2.slice().sort((a,b) => a.precio - b.precio);
-
-// console.log(productosOrdenados);
-
-// //////////////////////////////////////////////////////////////////////////////////
-
-
-// //preguntar si esta conforme con los productos
-
-// const CONSULTA = "si";
-// let intentos = 0;
-// let consulta= " ";
-
-// while(consulta !== CONSULTA){
-//     consulta = prompt("Estas conforme con tus productos?").toLowerCase();
-//     intentos++;
-
-//     if(consulta !== CONSULTA){
-//         alert("No te preocupes, cerra y volve al incio de pagina");
-//     }
-// }
-
-// alert("¡EXCELENTE! GRACIAS POR ELEGIRNOS" + " " + intentos + " " + "Consultas" );
-
 const productosAElegir = [
-    {id:1, nombre:"Set de Platillos Paiste 2002", precio:65000},
-    {id:2, nombre:"Set de Platillos Zildjian", precio:95000},
-    {id:3, nombre:"Set de Bateria DW Profesional", precio:130000},
-    {id:4, nombre:"Set Bateria TAMA Profesional", precio:110000},
+    {id:1, nombre:"Set de Platillos Paiste 2002", precio:67.00},
+    {id:2, nombre:"Set de Platillos Zildjian", precio:96.00},
+    {id:3, nombre:"Set de Bateria DW Profesional", precio:129.00},
+    {id:4, nombre:"Set Bateria TAMA Profesional", precio:110.00},
 ]
 function agregarProducto(id,nombre,precio){
     const carrito = JSON.parse(localStorage.getItem(`carrito`)) || [];
@@ -174,7 +48,7 @@ function cards(){
         productoElement.classList.add(`producto`);
         productoElement.innerHTML = `
         <h3>${producto.nombre}</h3>
-        <p>${"$" + producto.precio}</p>
+        <p>${"USD" + producto.precio}</p>
         <button onclick ="agregarProducto('${producto.id}','${producto.nombre}','${producto.precio}')">Agregar al carrito</button>
         
 
@@ -187,6 +61,59 @@ function cards(){
 
 mostrarCarrito();
 cards();
+
+//cotizador//
+
+
+const cotizador = "https://criptoya.com/api/dolar";
+
+const divCotizador = document.getElementById("divCotizador");
+
+setInterval(()=>{
+    fetch(cotizador)
+    .then(response => response.json())
+    .then(({oficial, blue})=>{
+        divCotizador.innerHTML = `
+        <h3>Valor del Dolar</h3>
+        <p>Dolar Oficial: ${oficial}</p>
+        <p>Dolar Blue: ${blue}</p>
+        `
+    })
+    .catch(error => console.log(error))
+
+
+
+},2000);
+
+const BOTON = document.getElementById("boton");
+
+let carrito = ["bateria", "platillos", "palillos"];
+
+BOTON.addEventListener("click", () => {
+    Swal.fire({
+        title: "¿Estas conforme con tu compra?",
+        icon: "warning",
+        confirmButtonText: "Si",
+        showCancelButton: true,
+        cancelButtonText: "No",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            carrito = carrito.filter((producto) => producto !== "bateria");
+            console.log(carrito);
+            Swal.fire({
+                title: "Perfecto!!! te enviamos la factura por mail",
+                icon: "success",
+                confirmButtonText: "aceptar",
+            });
+        } else {
+            Swal.fire({
+                title: "Cancelaste la compra",
+                icon: "error",
+                confirmButtonText: "Aceptar",
+            });
+        }
+    });
+});
 
 
 
